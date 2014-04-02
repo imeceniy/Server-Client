aef48fc65ae397bc329fbb27259b7b6c11c98853
imeceniy <lord220@me.com>
2014-04-02 12:21:12 +0400

    A main method

--- Server-Client/Program.cs
+++ Server-Client/Program.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Server_Client
 {
@@ -9,6 +13,56 @@ namespace Server_Client
     {
         static void Main(string[] args)
         {
+            TcpListener server = null;
+            try
+            {
+                // Определим нужное максимальное количество потоков
+                // Пусть будет по 4 на каждый процессор
+                int MaxThreadsCount = Environment.ProcessorCount * 4;
+                Console.WriteLine(MaxThreadsCount.ToString());
+                // Установим максимальное количество рабочих потоков
+                ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
+                // Установим минимальное количество рабочих потоков
+                ThreadPool.SetMinThreads(2, 2);
+
+
+                // Устанавливаем порт для TcpListener = 9595.
+                Int32 port = 9595;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                int counter = 0;
+                server = new TcpListener(localAddr, port);
+
+                // Запускаем TcpListener и начинаем слушать клиентов.
+                server.Start();
+
+                // Принимаем клиентов в бесконечном цикле.
+                while (true)
+                {
+
+                    Console.Write("\nWaiting for a connection... ");
+
+                    // При появлении клиента добавляем в очередь потоков его обработку.
+                    ThreadPool.QueueUserWorkItem(ObrabotkaZaprosa, server.AcceptTcpClient());
+                    // Выводим информацию о подключении.
+                    counter++;
+                    Console.Write("\nConnection №" + counter.ToString() + "!");
+
+                }
+            }
+            catch (SocketException e)
+            {
+                //В случае ошибки, выводим что это за ошибка.
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Останавливаем TcpListener.
+                server.Stop();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
         }
     }
 }
